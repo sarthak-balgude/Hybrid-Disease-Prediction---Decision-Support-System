1a0b01c7feed7063d604fa9d700d217d3103ab5f
devanshnegi88 <devanshnegi88@gmail.com>
2026-03-10 20:26:06 +0530

    updated main_routes

--- hybrid-disease-prediction-system/app/blueprints/main/routes.py
+++ hybrid-disease-prediction-system/app/blueprints/main/routes.py
@@ -4,4 +4,14 @@ from . import main_bp
 
 @main_bp.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home/home.html")
+
+
+@main_bp.route("/symptoms")
+def symptoms():
+    return render_template("home/symptoms.html")
+
+
+@main_bp.route("/result")
+def result():
+    return render_template("home/result.html")
